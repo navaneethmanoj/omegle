f2037441dd3580f522f0acc20769501190f8dcb8
navaneethmanoj <navaneethpm@gmail.com>
2024-02-06 11:08:54 +0530

    Init backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-const { Server } = require("socket.io");
+import { Socket } from "socket.io";
+
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection',(socket: Socket) => {
+    console.log("A user connected")
+})
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
